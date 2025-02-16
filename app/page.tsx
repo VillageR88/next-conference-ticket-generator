316c0632162718c9f1f64485498922266d56b568
VillageR88 <villager88pl@gmail.com>
2025-02-16 01:08:07 +0100

    Initial error
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,32 @@
 'use client';
 import Image from 'next/image';
 import iconUpload from '../public/assets/images/icon-upload.svg';
-import iconInfo from '../public/assets/images/icon-info.svg';
 import { useRouter } from 'next/navigation';
 import { useRef, useState } from 'react';
 
 export default function Home(): JSX.Element {
   const router = useRouter();
   const [avatar, setAvatar] = useState<File | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const fileInput = useRef<null | HTMLInputElement>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     if (event.target.files?.[0]) {
-      setAvatar(event.target.files[0]);
+      const file = event.target.files[0];
+      if (file.size > 500 * 1024) {
+        setErrorMessage('File too large. Please upload a photo under 500KB.');
+        setAvatar(null);
+      } else {
+        setErrorMessage(null);
+        setAvatar(file);
+      }
       event.target.value = ''; // Reset the input value to allow re-upload
     }
   };
 
   const handleRemoveImage = (event: React.MouseEvent<HTMLButtonElement>): void => {
     setAvatar(null);
+    setErrorMessage(null);
     event.preventDefault();
   };
 
@@ -51,6 +59,7 @@ export default function Home(): JSX.Element {
             <div className="relative flex h-[126px] w-full flex-col items-center justify-center gap-[16px] overflow-hidden rounded-[12px] bg-[rgba(255,255,255,0.08)] bg-[url(../public/assets/images/dashed-border.svg)] outline-offset-4 [transition:outline_300ms,background-color_300ms] has-[input:focus-visible]:outline has-[input:focus-visible]:outline-[#8784A5] [&:hover>div]:border-[#8784A5] [&:not(:has(button:hover))]:hover:bg-[rgba(255,255,255,0.2)]">
               <input
                 ref={fileInput}
+                required={!avatar}
                 id="file-input"
                 type="file"
                 accept="image/*"
@@ -94,15 +103,42 @@ export default function Home(): JSX.Element {
               )}
             </div>
             <div className="flex items-center gap-[8px]">
-              <Image alt="info icon" src={iconInfo as string} />
-              <p className="text-[12px] leading-[120%] tracking-[-0.2px] text-[#D1D0D5]">
-                Upload your photo (JPG or PNG, max size: 500KB).
-              </p>
+              <svg
+                aria-label="info icon"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="none"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  stroke={errorMessage ? '#F57463' : '#D1D0D5'}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z"
+                />
+                <path fill={errorMessage ? '#F57463' : '#D1D0D5'} d="M8.004 10.462V7.596ZM8 5.57v-.042Z" />
+                <path
+                  stroke={errorMessage ? '#F57463' : '#D1D0D5'}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.004 10.462V7.596M8 5.569v-.042"
+                />
+              </svg>
+              {errorMessage ? (
+                <p className="text-[12px] leading-[120%] tracking-[-0.2px] text-[#F57463]">{errorMessage}</p>
+              ) : (
+                <p className="text-[12px] leading-[120%] tracking-[-0.2px] text-[#D1D0D5]">
+                  Upload your photo (JPG or PNG, max size: 500KB).
+                </p>
+              )}
             </div>
           </label>
           <label className="flex flex-col gap-[12px] text-white">
             <h2>Full Name</h2>
             <input
+              required
               id="name"
               autoComplete="name"
               type="text"
@@ -112,16 +148,19 @@ export default function Home(): JSX.Element {
           <label className="flex flex-col gap-[12px] text-white">
             <h2>Email Address</h2>
             <input
+              required
               id="email"
               autoComplete="email"
               type="email"
               placeholder="example@email.com"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
               className="h-[54px] w-full rounded-[12px] bg-[rgba(255,255,255,0.08)] px-[16px] text-[18px] leading-[120%] text-white outline-none [border:1px_solid_#8784A5] [transition:outline_300ms,background-color_300ms] hover:bg-[rgba(255,255,255,0.2)] focus:outline-offset-4 focus:outline-[#8784A5]"
             />
           </label>
           <label className="flex flex-col gap-[12px] text-white">
             <h2>GitHub Username</h2>
             <input
+              required
               id="github"
               autoComplete="username"
               type="text"
